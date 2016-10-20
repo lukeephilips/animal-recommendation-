631965461f8b587022bf620cd22f3f0c205fb8a6
Jonathan Thom and Luke Philips <luke@>
2016-10-20 08:36:07 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var animalselection = $("#giraffe-button").click();
+  var animalselection = $("hippo-button").click();
+  var animalselection = $("elephant-button").click();
+
+  if anim
+});
