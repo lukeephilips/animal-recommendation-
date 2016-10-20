6cc8d56b70ab5c83f6a47a61cfcfd8d7b9c6bdfc
Jonathan Thom and Luke Philips <jonathan@>
2016-10-20 09:12:52 -0700

    Javascript click functionality and page content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
-  var animalselection = $("#giraffe-button").click();
-  var animalselection = $("hippo-button").click();
-  var animalselection = $("elephant-button").click();
+  var animalselection = "";
 
-  if anim
+  $("#elephant-button").click(function() {
+     animalselection = "elephant";
+     $(".homepage").hide();
+     console.log(animalselection);
+  });
+  $("#hippo-button").click(function() {
+     animalselection = "hippo";
+     $(".homepage").hide();
+     console.log(animalselection);
+  });
+   $("#giraffe-button").click(function() {
+     animalselection = "giraffe";
+     $(".homepage").hide();
+      console.log(animalselection);
+   });
+
+   $(document).click(function() {
+     console.log("page running" + animalselection)
+
+      if (animalselection === "elephant") {
+       $("#elephantpage").show();
+     }
+     else if (animalselection === "hippo") {
+       $("#hippopage").show();
+     }
+     else if (animalselection === "giraffe") {
+       $("#giraffepage").show();
+       console.log(animalselection + "text");
+     }
+    else {
+    }
+  });
+
 });
